ac8d88563e04692c1dd3a27cfb8418198f61848a
Michal Belak <mbelak@datamole.cz>
2019-11-30 00:04:40 +0100

    reinforce and reinforce_baseline done

--- labs/07/reinforce.py
+++ labs/07/reinforce.py
@@ -1,3 +1,6 @@
+# dd7e3410-38c0-11e8-9b58-00505601122b
+# 6e14ef6b-3281-11e8-9de3-00505601122b
+
 #!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
@@ -6,46 +9,53 @@ import cart_pole_evaluator
 
 class Network:
     def __init__(self, env, args):
-        # TODO: Create a suitable network, using Adam optimizer with given
-        # `args.learning_rate`. The network should predict distribution over
-        # possible actions.
+        # TODO: Define suitable model. The inputs have shape `env.state_shape`,
+        # and the model should produce probabilities of `env.actions` actions.
+        #
+        # You can use for example one hidden layer with `args.hidden_layer`
+        # and some non-linear activation. It is possible to use a `Sequential`
+        # model, and to use `compile`, `train_on_batch` and `predict_on_batch`
+        # methods.
+        #
+        # Use Adam optimizer with given `args.learning_rate`.
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Dense(args.hidden_layer, activation = tf.nn.tanh, input_shape = env.state_shape))
+        #self.model.add(tf.keras.layers.Dense(args.hidden_layer, activation = tf.nn.sigmoid))
+        self.model.add(tf.keras.layers.Dense(env.actions, activation = tf.nn.softmax))
 
-        # Warning: If you plan to use Keras `.train_on_batch` and/or `.predict_on_batch`
-        # methods, pass `experimental_run_tf_function=False` to compile. There is
-        # a bug in TF 2.0 which causes the `*_on_batch` methods not to use `tf.function`.
+        self.model.compile(
+            optimizer = tf.optimizers.Adam(args.learning_rate), 
+            loss      = 'sparse_categorical_crossentropy',
+            experimental_use_tf_function=False
+        )
 
-        # Otherwise, if you are training manually, using `tf.function` is a good idea
-        # to get good performance.
-        raise NotImplementedError()
+
+
 
     def train(self, states, actions, returns):
-        states, actions, returns = np.array(states, np.float32), np.array(actions, np.int32), np.array(returns, np.float32)
-
+        states, actions, returns = np.array(states), np.array(actions), np.array(returns)
         # TODO: Train the model using the states, actions and observed returns.
-        # Use `returns` as weights in the sparse crossentropy loss.
-        raise NotImplementedError()
+        self.model.train_on_batch(states, actions, sample_weight = returns)
+
 
     def predict(self, states):
-        states = np.array(states, np.float32)
-        # TODO: Predict distribution over actions for the given input states.
-        raise NotImplementedError()
-
+        states = np.array(states)
+        # TODO: Predict distribution over actions for the given input states
+        return self.model.predict_on_batch(states)
 
 if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Number of episodes to train on.")
-    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
-    parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-    parser.add_argument("--hidden_layers", default=None, type=int, help="Number of hidden layers.")
-    parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-    parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+    parser.add_argument("--batch_size", default=32, type=int, help="Number of episodes to train on.")
+    parser.add_argument("--episodes", default=700, type=int, help="Training episodes.")
+    parser.add_argument("--hidden_layer", default=80, type=int, help="Size of hidden layer.")
+    parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
 
-    # Fix random seeds and number of threads
+    # Fix random seed
     np.random.seed(42)
     tf.random.set_seed(42)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
@@ -68,10 +78,10 @@ if __name__ == "__main__":
                 if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                     env.render()
 
-                # TODO: Compute action probabilities using `network.predict` and current `state`
-
-                # TODO: Choose `action` according to `probabilities` distribution (np.random.choice can be used)
-
+                probabilities = network.predict([state])[0]
+                # TODO: Compute `action` according to the distribution returned by the network.
+                # The `np.random.choice` method comes handy.
+                action = int(np.random.uniform() > probabilities[0])
                 next_state, reward, done, _ = env.step(action)
 
                 states.append(state)
@@ -80,19 +90,27 @@ if __name__ == "__main__":
 
                 state = next_state
 
-            # TODO: Compute returns by summing rewards (with discounting)
+            # TODO: Compute `returns` from the observed `rewards`.
+            returns = []
+            g = 0
+            lambd = 1
+            for i in range(1, len(rewards)):
+                reward = rewards[-i]
+                g = lambd * g + reward
+                returns.append(g)
+            returns.append(0)
+            returns.reverse()
 
-            # TODO: Add states, actions and returns to the training batch
+            batch_states += states
+            batch_actions += actions
+            batch_returns += returns
 
-        # Train using the generated batch
         network.train(batch_states, batch_actions, batch_returns)
 
     # Final evaluation
     while True:
         state, done = env.reset(True), False
         while not done:
-            # TODO: Compute action `probabilities` using `network.predict` and current `state`
-
-            # Choose greedy action this time
+            probabilities = network.predict([state])[0]
             action = np.argmax(probabilities)
             state, reward, done, _ = env.step(action)
--- labs/07/reinforce_baseline.py
+++ labs/07/reinforce_baseline.py
@@ -1,20 +1,43 @@
+# dd7e3410-38c0-11e8-9b58-00505601122b
+# 6e14ef6b-3281-11e8-9de3-00505601122b
+
 #!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
 
 import cart_pole_evaluator
 
+
 class Network:
     def __init__(self, env, args):
         # TODO: Define suitable model. Apart from the model defined in `reinforce`,
-        # define also another model `baseline`, which produces one output
-        # (using a dense layer without activation).
+        # define also another model `baseline`, which processes the input using
+        # a fully connected hidden layer with non-linear activation, and produces
+        # one output (using a dense layer without activation).
         #
         # Use Adam optimizer with given `args.learning_rate` for both models.
-        raise NotImplementedError()
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Dense(args.hidden_layer_size, activation = tf.nn.tanh, input_shape = env.state_shape))
+        self.model.add(tf.keras.layers.Dense(env.actions, activation = tf.nn.softmax))
+
+        self.base_line = tf.keras.Sequential()
+        self.base_line.add(tf.keras.layers.Dense(args.hidden_layer_size, activation = tf.nn.tanh, input_shape = env.state_shape))
+        self.base_line.add(tf.keras.layers.Dense(1))
+
+        self.model.compile(
+            optimizer = tf.optimizers.Adam(args.learning_rate), 
+            loss      = 'sparse_categorical_crossentropy',
+            experimental_use_tf_function=False
+        )
+
+        self.base_line.compile(
+            optimizer = tf.optimizers.Adam(args.learning_rate), 
+            loss      = 'mean_squared_error',
+            experimental_use_tf_function=False
+        )
 
     def train(self, states, actions, returns):
-        states, actions, returns = np.array(states, np.float32), np.array(actions, np.int32), np.array(returns, np.float32)
+        states, actions, returns = np.array(states), np.array(actions), np.array(returns)
 
         # TODO: Train the model using the states, actions and observed returns.
         # You should:
@@ -22,30 +45,35 @@ class Network:
         # - train the policy model, using `returns - predicted_baseline` as weights
         #   in the sparse crossentropy loss
         # - train the `baseline` model to predict `returns`
-        raise NotImplementedError()
+        predicted_baseline = self.base_line.predict_on_batch(states)
+        predicted_baseline = np.reshape(predicted_baseline, returns.shape)
+        self.model.train_on_batch(states, actions, sample_weight = returns - predicted_baseline)
+
+        self.base_line.train_on_batch(states, returns)
 
     def predict(self, states):
-        states = np.array(states, np.float32)
+        states = np.array(states)
+
         # TODO: Predict distribution over actions for the given input states. Return
         # only the probabilities, not the baseline.
-        raise NotImplementedError()
+        return self.model.predict_on_batch(states)
 
 
 if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Number of episodes to train on.")
-    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
-    parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-    parser.add_argument("--hidden_layers", default=None, type=int, help="Number of hidden layers.")
-    parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-    parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+    parser.add_argument("--batch_size", default=4, type=int, help="Number of episodes to train on.")
+    parser.add_argument("--episodes", default=200, type=int, help="Training episodes.")
+    parser.add_argument("--gamma", default=1, type=float, help="Discounting factor.")
+    parser.add_argument("--hidden_layers", default=1, type=int, help="Number of hidden layers.")
+    parser.add_argument("--hidden_layer_size", default=95, type=int, help="Size of hidden layer.")
+    parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-    args = parser.parse_args()
+    args = parser.parse_args([])
 
-    # Fix random seeds and number of threads
+    # Fix random seed
     np.random.seed(42)
     tf.random.set_seed(42)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
@@ -68,11 +96,10 @@ if __name__ == "__main__":
                 if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                     env.render()
 
-                # TODO(reinforce): Compute action probabilities using
-                # `network.predict` and current `state`
-
-                # TODO(reinforce): Choose `action` according to `probabilities` distribution (np.random.choice can be used)
-
+                probabilities = network.predict([state])[0]
+                # TODO(reinforce): Compute `action` according to the distribution returned by the network.
+                # The `np.random.choice` method comes handy.
+                action = int(np.random.uniform() > probabilities[0])
                 next_state, reward, done, _ = env.step(action)
 
                 states.append(state)
@@ -81,19 +108,23 @@ if __name__ == "__main__":
 
                 state = next_state
 
-            # TODO(reinforce): Compute returns by summing rewards (with discounting)
+            # TODO(reinforce): Compute `returns` from the observed `rewards`.
+            returns = []
+            g = 0
+            for i in range(len(rewards)):
+                returns.append(sum(rewards[i:]))
 
-            # TODO(reinforce): Add states, actions and returns to the training batch
+            batch_states += states
+            batch_actions += actions
+            batch_returns += returns
 
-        # Train using the generated batch
+        network.train(batch_states, batch_actions, batch_returns)
         network.train(batch_states, batch_actions, batch_returns)
 
     # Final evaluation
     while True:
         state, done = env.reset(True), False
         while not done:
-            # TODO(reinforce): Compute action `probabilities` using `network.predict` and current `state`
-
-            # Choose greedy action this time
+            probabilities = network.predict([state])[0]
             action = np.argmax(probabilities)
             state, reward, done, _ = env.step(action)
